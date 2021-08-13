2ef5a0c89a5a5abf49f9c29ead92aef48beae403
Alphonzo Escolar <phonz.dev@gmail.com>
2021-08-13 18:25:59 +0800

    Add #parse_pos and #parse_val

--- sudoku.rb
+++ sudoku.rb
@@ -1,7 +1,4 @@
 require_relative "board"
-require 'colorize'
-
-puts "Only contractors write code this bad.".yellow
 
 class SudokuGame
   def self.from_file(filename)
@@ -10,16 +7,7 @@ class SudokuGame
   end
 
   def initialize(board)
-    @board = [[]]
-  end
-
-  def method_missing(method_name, *args)
-    if method_name =~ /val/
-      Integer(1)
-    else
-      string = args[0]
-      string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
-    end
+    @board = board
   end
 
   def get_pos
@@ -31,7 +19,6 @@ class SudokuGame
       begin
         pos = parse_pos(gets.chomp)
       rescue
-        # TODO: Google how to print the error that happened inside of a rescue statement.
         puts "Invalid position entered (did you use a comma?)"
         puts ""
 
@@ -49,6 +36,14 @@ class SudokuGame
       val = parse_val(gets.chomp)
     end
     val
+  end
+
+  def parse_pos(pos)
+    pos.split(",").map(&:to_i)
+  end
+
+  def parse_val(val)
+    Integer(val)
   end
 
   def play_turn
@@ -85,3 +80,4 @@ end
 
 
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
